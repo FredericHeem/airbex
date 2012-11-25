2a1532e2c93c799298dd842c704bbe83771e18f4
abrkn <a@abrkn.com>
2012-11-25 16:12:15 +0100

    increase volume to 1 for now, only 1 trader (fx)

--- bin/trader.js
+++ bin/trader.js
@@ -7,33 +7,16 @@ var Snow = require('snow-client')
 , Fx = require('../lib/fx')
 , config = require('../config');
 
-var snowMtgox = new Snow(config('snow-mtgox'));
-
-snowMtgox.init(function(err) {
-    if (err) return console.error('init failed', err);
-    var mtgox = new Mtgox(config('mtgox'));
-
-    var marginUsd = new Margin('BTCUSD', mtgox, snowMtgox);
-    var marginGbp = new Margin('BTCGBP', mtgox, snowMtgox);
-});
-
-var snowBitfloor = new Snow(config('snow-bitfloor'));
-
-snowBitfloor.init(function(err) {
-    if (err) return console.error('init failed', err);
-    var bitfloor = new Bitfloor(config('bitfloor'));
-
-    var margin = new Margin('BTCUSD', bitfloor, snowBitfloor);
-});
-
 var snowFx = new Snow(config('snow-fx'));
 
 snowFx.init(function(err) {
     if (err) return console.error('init failed', err);
 
-    var fxBtcNokUsd = new Fx(snowFx, 'BTC', 'NOK', 'USD');
+    var gox = new Mtgox(config('mtgox'))
+
+    var fxBtcNokUsd = new Fx(gox, 'BTC', 'NOK', 'USD');
     var marginBtcNokUsd = new Margin('BTCNOK', fxBtcNokUsd, snowFx);
 
-    var fxBtcNokGbp = new Fx(snowFx, 'BTC', 'NOK', 'GBP');
-    var marginBtcNokGbp = new Margin('BTCNOK', fxBtcNokGbp, snowFx);
+/*    var fxBtcNokGbp = new Fx(snowFx, 'BTC', 'NOK', 'GBP');
+    var marginBtcNokGbp = new Margin('BTCNOK', fxBtcNokGbp, snowFx);*/
 });

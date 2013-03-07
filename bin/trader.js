9205e088a4cf975de9bbba30a3f88e8ab60154ae
abrkn <a@abrkn.com>
2013-03-07 13:17:31 +0700

    add ripple

--- bin/trader.js
+++ bin/trader.js
@@ -1,22 +1,80 @@
-var Snow = require('snow-client')
-, Bitfloor = require('bitfloor')
-, Margin = require('../lib/margin')
+//var Snow = require('snow')
+//, Bitfloor = require('bitfloor')
+var Margin = require('../lib/margin')
 , debug = require('debug')('snow:trader')
-, Mtgox = require('mtgox')
+, MtGox = require('mtgox')
 , _ = require('underscore')
 , Fx = require('../lib/fx')
-, config = require('../config');
+, Combiner = require('../lib/combiner')
+, config = require('../config')
 
-var snowFx = new Snow(config('snow-fx'));
+var gox = new MtGox(config('mtgox'))
+//var snow = new Snow(config('snow'))
+//var bitfloor = new Bitfloor(config('bitfloor'))
 
-snowFx.init(function(err) {
-    if (err) return console.error('init failed', err);
+var Wave = require('rwave')
+var wave = new Wave(config('ripple'))
 
-    var gox = new Mtgox(config('mtgox'))
+// combine BTC USD
+var combiner = new Combiner([
+    {
+        pair: {
+            base: { currency: 'BTC' },
+            quote: { currency: 'USD' }
+        },
+        client: gox
+    }/*,
+    {
+        // BTC USD bitfloor
+        pair: {
+            base: { currency: 'BTC' },
+            quote: { currency: 'USD' }
+        },
+        client: bitfloor
+    },
+    {
+        // BTC NOK (virtual via USD)
+        pair: {
+            base: { currency: 'BTC' },
+            quote: { currency: 'NOK' }
+        },
+        client: new Fx(gox, 'BTC', 'NOK', 'USD')
+    },
+    {
+        // snowcoin BTC XRP
+        pair: {
+            base: { currency: 'BTC' },
+            quote: { currency: 'XRP' }
+        },
+        client: snow
+    }*/
+])
 
-    var fxBtcNokUsd = new Fx(gox, 'BTC', 'NOK', 'USD');
-    var marginBtcNokUsd = new Margin('BTCNOK', fxBtcNokUsd, snowFx);
+var traders = [
+    // combiner --> bitstamp btc to bitstamp usd
+    new Margin({
+        base: { currency: 'BTC', issuer: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B' },
+        quote: { currency: 'USD', issuer: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B' }
+    }, combiner, wave, {
+        volume: 0.1,
+        margin: 0.01
+    }),
 
-/*    var fxBtcNokGbp = new Fx(snowFx, 'BTC', 'NOK', 'GBP');
-    var marginBtcNokGbp = new Margin('BTCNOK', fxBtcNokGbp, snowFx);*/
-});
+    // combiner --> bitstamp btc to wix btc
+    new Margin({
+        base: { currency: 'BTC', issuer: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B' },
+        quote: { currency: 'BTC', issuer: 'r9vbV3EHvXWjSkeQ6CAcYVPGeq7TuiXY2X' }
+    }, null, wave, {
+        volume: 0.1,
+        margin: 0.01
+    }),
+
+    // combiner --> bitstamp btc to weex usd
+    new Margin({
+        base: { currency: 'BTC', issuer: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B' },
+        quote: { currency: 'USD', issuer: 'r9vbV3EHvXWjSkeQ6CAcYVPGeq7TuiXY2X' }
+    }, combiner, wave, {
+        volume: 0.1,
+        margin: 0.01
+    })
+]

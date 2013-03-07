ebd0e2afbd4eeb0f0f7c20a3a2218469fd596065
abrkn <a@abrkn.com>
2013-03-07 21:01:27 +0700

    remove obsolete ripplein tests

--- test/ripplein.js
+++ test/ripplein.js
@@ -16,32 +16,4 @@ describe('RippleIn', function() {
             .done()
         })
     })
-
-    describe('parseAmount', function() {
-        it('uses the scale', function() {
-            var ri = { securities: { XRP: { scale: 8 }, BTC: { scale: 8 }, USD: { scale: 5 } } }
-            , actual = RippleIn.prototype.parseAmount.call(ri, '1.97582', 'BTC')
-            expect(actual).to.be(197582000)
-
-            actual = RippleIn.prototype.parseAmount.call(ri, '0.00000001', 'XRP')
-            expect(actual).to.be(1)
-
-            actual = RippleIn.prototype.parseAmount.call(ri, '235.9', 'USD')
-            expect(actual).to.be(23590000)
-        })
-
-        it('throws when precision is too high', function() {
-            var ri = { securities: { USD: { scale: 5 } } }
-            expect(function() {
-                RippleIn.prototype.parseAmount.call(ri, '1.975823', 'USD')
-            }).to.throwError(/precision/)
-        })
-
-        it('throws when the security is not known', function() {
-            var ri = { securities: { USD: { scale: 5 } } }
-            expect(function() {
-                RippleIn.prototype.parseAmount.call(ri, '1.975823', 'GBP')
-            }).to.throwError(/found/)
-        })
-    })
 })

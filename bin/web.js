6d72f033d951133ff09a0d4a4d89c948ad415d76
Andreas Brekken <a@abrkn.com>
2013-03-10 16:52:42 +0100

    remove bin

--- bin/web.js
+++
@@ -1,6 +0,0 @@
-process.env.DEBUG || (process.env.DEBUG = '.*');
-
-console.log('starting web app');
-
-var config = require('../config');
-var app = new (require('../lib/server/App'))(process.env.PORT || config('port'));

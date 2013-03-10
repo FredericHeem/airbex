2cd16042f7c6c0eb2074e917dbdcca8291af6abe
Andreas Brekken <a@abrkn.com>
2013-03-10 09:13:50 +0100

    new config concept with more env

--- config/index.js
+++ config/index.js
@@ -1,3 +1,19 @@
-var path = require('path')
-, env = process.env.NODE_ENV || 'dev'
-module.exports = require('./config.' + env + '.json')
+var debug = require('debug')('config')
+, _ = require('underscore')
+, path = require('path')
+, fs = require('fs')
+, argv = require('optimist').argv
+
+module.exports = {
+}
+
+if (!_.isUndefined(process.env.NODE_ENV)) {
+	var fn = path.join(__dirname, process.env.NODE_ENV)
+	if (fs.fileExists(fn + '.json')) {
+		_.extend(module.exports, require(fn + '.json'))
+	} else if (fs.fileExists(fn + '.js')) {
+		_.extend(module.exports, require(fn + '.js'))
+	}
+}
+
+_.extend(module.exports, argv)

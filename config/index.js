978462653114bd0effb739ac2e88870bb6ea0b0a
Andreas Brekken <a@abrkn.com>
2013-03-10 12:17:10 +0100

    moving to new config format

--- config/index.js
+++ config/index.js
@@ -1,5 +1,4 @@
-var debug = require('debug')('config')
-, _ = require('underscore')
+var _ = require('underscore')
 , path = require('path')
 , fs = require('fs')
 , argv = require('optimist').argv
@@ -7,11 +6,11 @@ var debug = require('debug')('config')
 module.exports = {
 }
 
-if (!_.isUndefined(process.env.NODE_ENV)) {
+if (typeof process.env.NODE_ENV === 'undefined') {
 	var fn = path.join(__dirname, process.env.NODE_ENV)
-	if (fs.fileExists(fn + '.json')) {
+	if (fs.existsSync(fn + '.json')) {
 		_.extend(module.exports, require(fn + '.json'))
-	} else if (fs.fileExists(fn + '.js')) {
+	} else if (fs.existsSync(fn + '.js')) {
 		_.extend(module.exports, require(fn + '.js'))
 	}
 }

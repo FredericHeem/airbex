082d58e1b3ef382841d84c368711344db86fa8db
Andreas Brekken <a@abrkn.com>
2013-03-10 12:29:34 +0100

    change to env config

--- config/index.js
+++ config/index.js
@@ -2,12 +2,14 @@ var _ = require('underscore')
 , path = require('path')
 , fs = require('fs')
 , argv = require('optimist').argv
+delete argv.$0
+delete argv._
 
 module.exports = {
 }
 
 if (typeof process.env.NODE_ENV === 'undefined') {
-	var fn = path.join(__dirname, process.env.NODE_ENV)
+	var fn = path.join(__dirname, 'config.' + (process.env.NODE_ENV || 'dev'))
 	if (fs.existsSync(fn + '.json')) {
 		_.extend(module.exports, require(fn + '.json'))
 	} else if (fs.existsSync(fn + '.js')) {

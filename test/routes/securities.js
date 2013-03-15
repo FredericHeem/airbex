2230cf79de5b507454b633b6e203da28a34d4870
Andreas Brekken <a@abrkn.com>
2013-03-15 16:17:14 +0100

    refactor securities route

---
+++ test/routes/securities.js
@@ -0,0 +1,36 @@
+var expect = require('expect.js')
+, securities = require(__filename.replace('test', 'lib'))
+
+describe('securities', function() {
+	describe('configure', function() {
+		it('adds expected routes', function() {
+			var routes = []
+			, app = {
+				get: function(url) { routes.push('get ' + url) }
+			}
+			securities.configure(app)
+			expect(routes).to.contain('get /public/securities')
+		})
+	})
+
+	describe('securities', function() {
+		it('returns the securities', function(done) {
+			var conn = {
+				query: function(q, c) {
+					expect(q).to.match(/from "security"/i)
+					c(null, { rows: [{ security_id: 'QQQ' }] })
+				}
+			}
+			, req = {
+			}
+			, res = {
+				send: function(r) {
+					expect(r[0].security_id).to.be('QQQ')
+					done()
+				}
+			}
+
+			securities.securities(conn, req, res, done)
+		})
+	})
+})

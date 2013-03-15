8f6a0a1bfcd7c2c2c24f8873e98f5d9438e9f31d
Andreas Brekken <a@abrkn.com>
2013-03-15 20:10:30 +0100

    add transactions.forUser

--- test/transactions.js
+++ test/transactions.js
@@ -10,12 +10,44 @@ describe('transactions', function() {
 				get: function(url) { routes.push('get ' + url) }
 			}
 			transactions.configure(app)
+			expect(routes).to.contain('get /accounts/transactions')
 			expect(routes).to.contain('get /accounts/:id/transactions')
 		})
 	})
 
-	describe('forUserAccount', function() {
+	describe('forUser', function() {
 		it('gets transactions for the user', function(done) {
+			var conn = {
+				query: function(q, cb) {
+					expect(q.text).to.be('SELECT * FROM account_transactions WHERE user_id = $1')
+					expect(q.values).to.eql([10])
+					cb(null, {
+						rows: [{
+							transaction_id: 5
+						}, {
+							transaction_id: 7
+						}]
+					})
+				}
+			}
+			, req = {
+				security: { userId: 10 }
+			}
+			, res = {
+				send: function(r) {
+					expect(r).to.be.an('array')
+					expect(r[0].transaction_id).to.be(5)
+					expect(r[1].transaction_id).to.be(7)
+					done()
+				}
+			}
+
+			transactions.forUser(conn, req, res, done)
+		})
+	})
+
+	describe('forUserAccount', function() {
+		it('gets transactions for the user account', function(done) {
 			var conn = {
 				query: function(q, cb) {
 					expect(q.text).to.be('SELECT * FROM account_transactions WHERE account_id = $1 AND user_id = $2')
--- transactions.js
+++ transactions.js
@@ -2,7 +2,21 @@ var Q = require('q')
 , transactions = module.exports = {}
 
 transactions.configure = function(app, conn, securityId) {
+    app.get('/accounts/transactions', transactions.forUser.bind(transactions, conn))
     app.get('/accounts/:id/transactions', transactions.forUserAccount.bind(transactions, conn))
+}
+
+transactions.forUser = function(conn, req, res, next) {
+    var query = 'SELECT * FROM account_transactions ' +
+        'WHERE user_id = $1'
+    Q.ninvoke(conn, 'query', {
+        text: query,
+        values: [req.security.userId]
+    })
+    .then(function(dres) {
+        res.send(dres.rows)
+    }, next)
+    .done()
 }
 
 transactions.forUserAccount = function(conn, req, res, next) {

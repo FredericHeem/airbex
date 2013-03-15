86573c74c48caa2a2edcaa228934e8d5e9c0c63a
Andreas Brekken <a@abrkn.com>
2013-03-15 15:52:50 +0100

    refactor users route

---
+++ test/routes/users.js
@@ -0,0 +1,42 @@
+var expect = require('expect.js')
+, users = require(__filename.replace('test', 'lib'))
+
+describe('users', function() {
+	describe('configure', function() {
+		it('adds expected routes', function() {
+			var routes = []
+			, app = {
+				post: function(url) { routes.push('post ' + url) }
+			}
+			users.configure(app)
+			expect(routes).to.contain('post /public/users')
+		})
+	})
+
+	describe('create', function() {
+		it('returns users', function(done) {
+			var conn = {
+				query: function(q, c) {
+					expect(q.text).to.match(/select create_user/i)
+					expect(q.values).to.eql(['key', 'secreto'])
+					c(null, { rows: [{ user_id: 89 }] })
+				}
+			}
+			, req = {
+				body: {
+					key: 'key',
+					secret: 'secreto'
+				}
+			}
+			, res = {
+				send: function(code, r) {
+					expect(code).to.be(201)
+					expect(r.user_id).to.be(89)
+					done()
+				}
+			}
+
+			users.create(conn, req, res, done)
+		})
+	})
+})

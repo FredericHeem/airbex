1714bee7806fc67da2c0e592e872fff2da921db2
Andreas Brekken <a@abrkn.com>
2013-03-19 16:58:09 +0700

    add whoami to find user from session

--- users.js
+++ users.js
@@ -2,7 +2,19 @@ var Q = require('q')
 , users = module.exports = {}
 
 users.configure = function(app, conn) {
+    app.get('/whoami', users.whoami.bind(users, conn))
     app.post('/public/users', users.create.bind(users, conn))
+}
+
+users.whoami = function(conn, req, res, next) {
+	conn.query({
+		text: 'SELECT email FROM "user" WHERE user_id = $1',
+		values: [req.security.userId]
+	}, function(err, dres) {
+		if (err) return next(err)
+		if (!dres.rows.length) return res.send(404)
+		res.send(dres.rows[0])
+	})
 }
 
 users.create = function(conn, req, res, next) {

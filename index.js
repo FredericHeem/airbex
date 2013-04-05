fe1fb56aba06b13ba5bfc699167a35bc228838bb
Andreas Brekken <a@abrkn.com>
2013-04-05 14:27:45 +0700

    add max age to cors headers

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     res.header('Access-Control-Allow-Headers', 'snow-key,snow-sign,Content-Type')
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365)
     return req.method == 'OPTIONS' ? res.send(200) : next()
 })
 

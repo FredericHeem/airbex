c641932422f97c2a0837d1c543f5e4dacbf5e103
Andreas Brekken <a@abrkn.com>
2013-04-12 18:05:55 +0700

    remove old transactions.create

--- transactions.js
+++ transactions.js
@@ -31,19 +31,3 @@ transactions.forUserAccount = function(conn, req, res, next) {
     }, next)
     .done()
 }
-
-transactions.create = function(conn, req, res, next) {
-    throw new Error('TODO: db function to verify debit/credit accounts')
-
-    var query = conn.build.insert('transaction', {
-        debit_account_id: req.body.debit_account_id,
-        credit_account_id: req.body.credit_account_id,
-        amount: req.body.amount
-    }, 'transaction_id')
-
-    Q.ninvoke(conn, 'query', query)
-    .then(function(cres) {
-        res.send({ transaction_id: cres.rows[0].transaction_id })
-    }, next)
-    .done()
-}

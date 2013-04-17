6624ca6e515464911daf723d0b38d6a4271a1b01
Andreas Brekken <a@abrkn.com>
2013-04-17 16:26:32 +0700

    fix scale of order matching

---
+++ migrations/030-match-base-volume-scale-fix.sql
@@ -0,0 +1,48 @@
+-- Bug: The bidder is credited with the wrong number of decimal places.
+-- Fix: Credit volume * 10^[book scale] instead of just volume
+
+CREATE OR REPLACE FUNCTION match_insert()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+    bido order%ROWTYPE;
+    asko order%ROWTYPE;
+    a bigint;
+    v bigint;
+    bs_scale int;
+    qs_scale int;
+    b book%ROWTYPE;
+BEGIN
+    SELECT * INTO bido FROM "order" WHERE order_id = NEW.bid_order_id;
+    SELECT * INTO asko FROM "order" WHERE order_id = NEW.ask_order_id;
+    SELECT * INTO b FROM book WHERE book_id = asko.book_id;
+
+    bs_scale := (SELECT scale FROM security WHERE security_id = b.base_security_id);
+    qs_scale := (SELECT scale FROM security WHERE security_id = b.quote_security_id);
+
+    UPDATE "order"
+    SET volume = volume - NEW.volume, matched = matched + NEW.volume
+    WHERE order_id = bido.order_id OR order_id = asko.order_id;
+
+    -- The book uses a volumes expressed in the scale of the security minus the scale of the book
+    RAISE NOTICE 'volume %, bs scale %, b scale %', NEW.volume, bs_scale, b.scale;
+    v := NEW.volume * 10^b.scale;
+
+    INSERT INTO transaction (debit_account_id, credit_account_id, amount)
+    VALUES (user_security_account(asko.user_id, b.base_security_id), user_security_account(bido.user_id, b.base_security_id), v);
+
+    IF random() < 0.5 THEN
+        a := ceil(NEW.price * NEW.volume / 10^(bs_scale - qs_scale));
+    ELSE
+        a := floor(NEW.price * NEW.volume / 10^(bs_scale - qs_scale));
+    END IF;
+
+    INSERT INTO transaction (debit_account_id, credit_account_id, amount)
+    VALUES (user_security_account(bido.user_id, b.quote_security_id), user_security_account(asko.user_id, b.quote_security_id), a);
+
+    RETURN NEW;
+END; $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION match_insert()
+  OWNER TO postgres;
---
+++ test/order-matching-incorrect-base-volume.sql
@@ -0,0 +1,68 @@
+-- Bob will sell 1 BTC @ 150,000 QRP
+-- Alice will buy 0.05 BTC @ 175,000 QRP
+-- Bob's order should remain with 0.95 volume (BTC)
+BEGIN; DO $$
+DECLARE
+    bob int;
+    bob_oid int;
+    alice_oid int;
+    alice int;
+    bid int;
+    mid int;
+BEGIN
+    INSERT INTO "security" (security_id, scale)
+    VALUES ('QRP', 6);
+
+    INSERT INTO "book" (base_security_id, quote_security_id, scale)
+    VALUES ('BTC', 'QRP', 3);
+
+    INSERT INTO "account" (security_id, "type")
+    VALUES ('QRP', 'edge');
+
+    bob := create_user('bob@gmail.com', 'bob', 'secret');
+    alice := create_user('alice@hotmail.com', 'alice', 'secret');
+    bid := (SELECT book_id FROM book WHERE base_security_id = 'BTC' AND quote_security_id = 'QRP');
+
+    -- Fund Bob with 10 BTC
+    INSERT INTO "transaction" (debit_account_id, credit_account_id, amount)
+    VALUES (special_account('edge', 'BTC'), user_security_account(bob, 'BTC'), 10e8);
+
+    -- Fund Alice with 10,000 QRP
+    INSERT INTO "transaction" (debit_account_id, credit_account_id, amount)
+    VALUES (special_account('edge', 'QRP'), user_security_account(alice, 'QRP'), 10000e6);
+
+    -- Create Bob's order
+    INSERT INTO "order" (book_id, user_id, side, price, volume)
+    VALUES (bid, bob, 1, 150000e3, 1*10^(8-3));
+
+    -- Bob, should now have a hold of 1 BTC
+    IF (SELECT hold FROM "account" WHERE account_id = user_security_account(bob, 'BTC')) <> 1e8 THEN
+        RAISE 'Mismatch held for Bob';
+    END IF;
+
+    -- Create Alice's order
+    INSERT INTO "order" (book_id, user_id, side, price, volume)
+    VALUES (bid, alice, 0, 175000e3, 0.05*10^(8-3));
+
+    mid := currval('match_match_id_seq');
+
+    -- Match should be 0.05 BTC @ 150,000 QRP (7,500 QRP)
+    IF (SELECT volume FROM "match" WHERE match_id = mid) <> 0.05*10^(8-3) THEN
+        RAISE 'Match volume incorrect';
+    END IF;
+
+    IF (SELECT price FROM "match" WHERE match_id = mid) <> 150000e3 THEN
+        RAISE 'Match price incorrect. Expeted 150000e3 actual %', (SELECT price FROM "match" WHERE match_id = mid);
+    END IF;
+
+    -- Bob should have 0.05 BTC * 150,000 QRP/BTC = 7,500 QRP
+    IF (SELECT balance FROM "account" WHERE account_id = user_security_account(bob, 'QRP')) <> 7500e6 THEN
+        RAISE 'Bob did not receive the correct amount of QRP';
+    END IF;
+
+    -- Alice should have 0.05 BTC
+    IF (SELECT balance FROM "account" WHERE account_id = user_security_account(alice, 'BTC')) <> 0.05e8 THEN
+        RAISE 'Alice did not receive the correct amount of BTC. Expected %, actual %',
+            0.05e8, (SELECT balance FROM "account" WHERE account_id = user_security_account(alice, 'BTC'));
+    END IF;
+END; $$; ROLLBACK;

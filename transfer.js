90eae85196134fc6e1ec2c07ee63ebcc46e13387
Andreas Brekken <a@abrkn.com>
2013-04-18 16:10:06 +0700

    add error message for transfer to non-existent user

--- transfer.js
+++ transfer.js
@@ -10,7 +10,17 @@ transfer.transfer = function(conn, req, res, next) {
         text: 'SELECT user_transfer_to_email($1, $2, $3, $4) transaction_id',
         values: [req.security.userId, req.body.email, req.body.security_id, req.body.amount]
     }, function(err, dres) {
-        if (err) return next(err)
+        if (err) {
+            if (err.message.match(/^User with email/)) {
+                return res.send(400, {
+                    name: 'UserNotFound',
+                    message: 'The user ' + req.body.email + ' does not exist'
+                })
+            }
+
+            return next(err)
+        }
+
         res.send({ transaction_id: dres.rows[0].transaction_id })
     })
 }

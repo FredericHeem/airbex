c63c5e9a08b2386512ad096ee7e424c60e407eaf
Andreas Brekken <a@abrkn.com>
2013-04-21 23:55:15 +0700

    improve login view. close #7

--- views/LoginView.js
+++ views/LoginView.js
@@ -13,19 +13,51 @@ var SectionView = require('./SectionView')
     },
 
     initialize: function() {
-        this.model = new Models.User({}, {
-            url: app.apiUrl + '/users'
-        })
+        this.model = new Models.User()
     },
 
     login: function(e) {
         e.preventDefault()
-        var email = this.$el.find('.email').val().toLowerCase()
-        , password = this.$el.find('.password').val()
 
-        this.trigger('login', {
-            hashes: app.hashCredentials(email, password)
+        var that = this
+        , email = this.$el.find('.email').val().toLowerCase()
+        , password = this.$el.find('.password').val()
+        , hashes = app.hashCredentials(email, password)
+        , result = this.model.fetch({
+            url: app.apiUrl + '/whoami',
+            headers: app.apiHeaders(null, hashes.key, hashes.secret)
         })
+
+        if (!result) {
+            return alert(this.model.validationError)
+        }
+
+        this.toggleInteraction(false)
+
+        result.then(function() {
+            app.user = that.model
+            app.credentials = hashes
+            $('body').addClass('is-logged-in')
+            $('#top .account-summary .logged-in .email').html(that.model.get('email'))
+
+            Backbone.history.navigate('my/accounts', true)
+        }, function(xhr) {
+            that.toggleInteraction(true)
+
+            var error = app.errorFromXhr(xhr)
+
+            if (error.code == 'UnknownApiKey' || error.code == 'BadMessageSignature') {
+                return alert('Wrong username/password')
+            }
+
+            alert(JSON.stringify(error, null, 4))
+        })
+    },
+
+    toggleInteraction: function(value) {
+        this.$el.find('input button')
+        .prop('disabled', !value)
+        .toggleClass('disabled', !value)
     },
 
     render: function() {

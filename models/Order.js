9bb85a0a3a8e4ff306b94761a358c0c68ececd43
Andreas Brekken <a@abrkn.com>
2013-04-29 13:30:26 +0200

    fix id attribute of order model

--- models/Order.js
+++ models/Order.js
@@ -1,5 +1,4 @@
 var Relational = require('backbone-relational')
 , num = require('num')
 , Order = module.exports = Relational.RelationalModel.extend({
-    idAttribute: 'order_id'
 })
--- test/models/Order.js
+++ test/models/Order.js
@@ -2,8 +2,4 @@ var expect = require('expect.js')
 , Order = require('../../models/Order')
 
 describe('Order', function() {
-    it('has idAttribute', function() {
-        var target = new Order({ order_id: 123 })
-        expect(target.id).to.be(123)
-    })
 })

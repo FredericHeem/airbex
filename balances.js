77be34ae9755b5e115be46b67069de8775128469
Andreas Brekken <a@abrkn.com>
2013-04-29 18:03:57 +0200

    rename currency_id to currency from balances

--- balances.js
+++ balances.js
@@ -8,7 +8,7 @@ Balances.configure = function(app, conn, auth) {
 Balances.forUser = function(conn, req, res, next) {
     Q.ninvoke(conn, 'query', {
         text: [
-            'SELECT currency_id, SUM(available_decimal) available',
+            'SELECT currency_id currency, SUM(available_decimal) available',
             'FROM account_view',
             'WHERE user_id = $1',
             'GROUP BY user_id, currency_id'

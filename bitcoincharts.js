a1e70c6408147e6fd4d15901f50ec6316b3df589
Andreas Brekken <a@abrkn.com>
2013-04-29 09:47:09 +0200

    rename book to market for bitcoincharts

--- bitcoincharts.js
+++ bitcoincharts.js
@@ -18,9 +18,9 @@ bitcoincharts.trades = function(conn, req, res, next) {
             'match_id tid ' +
             'FROM match_view m ' +
             'INNER JOIN "order" bo ON bo.order_id = m.bid_order_id ' +
-            'INNER JOIN "book" b ON b.book_id = bo.book_id ' +
-            'INNER JOIN "currency" bs ON b.base_currency_id = bs.currency_id ' +
-            'WHERE b.quote_currency_id = $1 AND m.match_id > $2 ' +
+            'INNER JOIN market m ON m.market_id = bo.market_id ' +
+            'INNER JOIN "currency" bs ON m.base_currency_id = bs.currency_id ' +
+            'WHERE m.quote_currency_id = $1 AND m.match_id > $2 ' +
             'ORDER BY m.match_id ASC;',
         values: [req.params.currencyId, since]
     })
@@ -35,8 +35,8 @@ bitcoincharts.orderbook = function(conn, req, res, next) {
         text: [
             'SELECT *',
             'FROM order_depth_view od',
-            'INNER JOIN book b ON b.book_id = od.book_id',
-            'WHERE b.base_currency_id = \'BTC\' AND b.quote_currency_id = $1'
+            'INNER JOIN market m ON m.market_id = od.market_id',
+            'WHERE m.base_currency_id = \'BTC\' AND m.quote_currency_id = $1'
         ].join('\n'),
         values: [req.params.currencyId]
     })

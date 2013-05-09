6118dd8cd7103d89c1161c8c4a1792910a5f59c6
Andreas Brekken <a@abrkn.com>
2013-05-09 18:10:44 +0200

    fix section swap

--- routes.js
+++ routes.js
@@ -14,58 +14,79 @@ var markets = require('./controllers/markets')
 , depositltc = require('./controllers/depositltc')
 , $app = $('body')
 
+function section(name) {
+    $app[0].className.split(/\s+/).forEach(function(name) {
+        if (!/is-section-/.test(name)) return
+        $app.removeClass(name)
+    })
+    $app.addClass('is-section-' + name)
+}
+
 module.exports = function(app, api, router) {
     var $section = $('#section')
 
     router
     .add(/^$/, function() {
         $section.html(home().$el)
+        section('home')
     })
     .add(/^markets$/, function() {
         $section.html(markets(app, api).$el)
+        section('markets')
     })
     .add(/^markets\/(.+)$/, function(id) {
         $section.html(market(app, api, id).$el)
+        section('market')
     })
     .add(/^activities$/, function() {
         $section.html(activities(app, api).$el)
+        section('activities')
     })
     .add(/^register$/, function() {
         $section.html(register(app, api).$el)
-        $app.addClass('is-section-register')
+        section('register')
     })
     .add(/^login(?:\?after=(.+))?$/, function(after) {
         $section.html(login(app, api, after).$el)
+        section('login')
     })
     .add(/^orders$/, function() {
         if (!app.authorize()) return
         $section.html(orders(app, api).$el)
+        section('orders')
     })
     .add(/^withdrawbtc$/, function() {
         if (!app.authorize()) return
         $section.html(withdrawbtc(app, api).$el)
+        section('withdrawbtc')
     })
     .add(/^withdrawltc$/, function() {
         if (!app.authorize()) return
         $section.html(withdrawltc(app, api).$el)
+        section('withdrawltc')
     })
     .add(/^withdrawripple$/, function() {
         if (!app.authorize()) return
         $section.html(withdrawripple(app, api).$el)
+        section('withdrawripple')
     })
     .add(/^dashboard$/, function() {
         if (!app.authorize()) return
         $section.html(dashboard(app, api).$el)
+        section('dashboard')
     })
     .add(/^depositbtc$/, function() {
         if (!app.authorize()) return
         $section.html(depositbtc(app, api).$el)
+        section('depositbtc')
     })
     .add(/^depositltc$/, function() {
         if (!app.authorize()) return
         $section.html(depositltc(app, api).$el)
+        section('depositltc')
     })
     .add(/^(.+)$/, function(hash) {
         $section.html(notfound(hash).$el)
+        section('notfound')
     })
 }

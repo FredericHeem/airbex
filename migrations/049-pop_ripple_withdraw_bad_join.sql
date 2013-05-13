7ba5f3040762888c68e649f00b61c5d3e0eb17c7
Andreas Brekken <a@abrkn.com>
2013-05-13 15:59:05 +0200

    fix pop ripple withdraw

---
+++ migrations/049-pop_ripple_withdraw_bad_join.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION pop_ripple_withdraw_requests()
+  RETURNS TABLE(request_id integer, amount bigint, currency_id currency_id, scale integer, address character varying) AS
+$BODY$
+BEGIN
+    RETURN QUERY
+    UPDATE withdraw_request wr
+    SET state = 'processing'
+    FROM
+        ripple_withdraw_request rwr,
+        account a,
+        currency c
+    WHERE
+        rwr.request_id = wr.request_id AND
+        a.account_id = wr.account_id AND
+        c.currency_id = a.currency_id AND
+        wr.request_id = (
+            SELECT MIN(rwr.request_id)
+            FROM
+                ripple_withdraw_request rwr
+            INNER JOIN
+                withdraw_request wr ON wr.request_id = rwr.request_id
+            INNER JOIN
+                account a ON a.account_id = wr.request_id
+            INNER JOIN
+                currency c ON c.currency_id = a.currency_id
+            WHERE
+                wr.state = 'requested'
+        )
+    RETURNING
+        wr.request_id,
+        wr.amount,
+        c.currency_id::currency_id,
+        c.scale,
+        rwr.address;
+END; $BODY$
+  LANGUAGE plpgsql VOLATILE;

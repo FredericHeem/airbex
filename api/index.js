97f1a0125666e573e4c1b7131a9fc51eed63d9c4
Andreas Brekken <a@abrkn.com>
2013-05-20 00:22:41 +0200

    add raven middleware before other handlers

--- api/index.js
+++ api/index.js
@@ -16,6 +16,11 @@ debug('starting api web server')
 app.config = config
 debug('config %j', config)
 
+if (config.raven) {
+    var raven = require('raven')
+    app.use(raven.middleware.express(config.raven))
+}
+
 app.use(express.bodyParser())
 
 var routes = ['bitcoincharts', 'v1', 'admin']
@@ -26,11 +31,6 @@ routes.forEach(function(name) {
 app.use(function(req, res) {
     res.send(404)
 })
-
-if (config.raven) {
-    var raven = require('raven')
-    app.use(raven.middleware.express(config.raven))
-}
 
 var cache = new Cache(conn, function(err) {
     if (err) throw err

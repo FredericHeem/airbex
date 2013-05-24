ad6f7e2185a4c96d21ff8394abbe16193b47dc79
Andreas Brekken <a@abrkn.com>
2013-05-24 11:16:13 +0200

    Add primary API key support. close #32

---
+++ db/test/060-primary-api-key.sql
@@ -0,0 +1,14 @@
+BEGIN; DO $$ <<fn>>
+DECLARE
+    uid int;
+    p boolean;
+BEGIN
+    uid := create_user('t@t', repeat('x', 64));
+    PERFORM replace_api_key(repeat('x', 64), repeat('y', 64));
+
+    p := (SELECT "primary" FROM api_key WHERE api_key_id = repeat('y', 64));
+
+    IF p <> TRUE THEN
+        RAISE 'New API was not made primary.';
+    END IF;
+END; $$; ROLLBACK;

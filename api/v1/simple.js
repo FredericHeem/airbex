09173b07626387be01b8f3715b9ca6e3c0920477
Andreas Brekken <a@abrkn.com>
2013-06-08 14:29:22 +0200

    Simple sell #ref 127

--- api/v1/simple.js
+++ api/v1/simple.js
@@ -1,6 +1,7 @@
 var _ = require('lodash')
 , validate = require('./validate')
 , activities = require('./activities')
+, async = require('async')
 , simple = module.exports = {}
 
 simple.configure = function(app, conn, auth) {
@@ -10,39 +11,63 @@ simple.configure = function(app, conn, auth) {
 simple.convertAndWithdraw = function(conn, req, res, next) {
     if (!validate(req.body, 'convertAndWithdraw', res)) return
 
-    var values = [
-        req.user,
-        +req.body.bankAccount,
-        req.app.cache.parseOrderVolume(req.body.amount, 'BTC' + req.body.currency),
-        'BTC',
-        req.body.currency
-    ]
+    async.waterfall([
+        function(next) {
+            conn.write.query({
+                text: [
+                    'SELECT convert_and_withdraw($1, $2, market_id, $3) wrid',
+                    'FROM market',
+                    'WHERE',
+                    '   base_currency_id = \'BTC\' AND',
+                    '   quote_currency_id = $4'
+                ].join('\n'),
+                values: [
+                    req.user,
+                    +req.body.bankAccount,
+                    req.app.cache.parseOrderVolume(req.body.amount, 'BTC' + req.body.currency),
+                    req.body.currency
+                ]
+            }, function(err, dr) {
+                if (err) {
+                    if (err.message.match(/non_negative_available/)) {
+                        return res.send(500, {
+                            name: 'NoFunds',
+                            message: 'Insufficient funds.'
+                        })
+                    }
 
-    console.log(values)
+                    return next(err)
+                }
 
-    conn.write.query({
-        text: [
-            'SELECT convert_and_withdraw($1, $2, $3, $4, $5)'
-        ].join('\n'),
-        values: values
-    }, function(err, dr) {
-        if (err) {
-            if (err.message.match(/non_negative_available/)) {
-                return res.send(500, {
-                    name: 'NoFunds',
-                    message: 'Insufficient funds.'
+                if (!dr.rowCount) {
+                    return next(new Error('Market not found'))
+                }
+
+                next(null, dr.rows[0].wrid)
+            })
+        },
+
+        function(wrid, next) {
+            conn.read.query({
+                text: [
+                    'SELECT amount',
+                    'FROM withdraw_request',
+                    'WHERE request_id = $1'
+                ].join('\n'),
+                values: [wrid]
+            }, function(err, dr) {
+                if (err) return next(err)
+
+                if (!dr.rowCount) {
+                    return next('Withdraw request ' + wrid + ' not found')
+                }
+
+                var amount = req.app.cache.formatCurrency(dr.rows[0].amount, req.body.currency)
+
+                return res.send(200, {
+                    amount:  amount
                 })
-            }
-            return next(err)
-        }
-
-        if (!dr.rowCount) {
-            return res.send(400, {
-                name: 'BankAccountNotFound',
-                message: 'Bank account not found for this user'
             })
         }
-
-        return res.send(204)
-    })
+    ], next)
 }

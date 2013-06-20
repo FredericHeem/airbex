ed490841cbfb83eee808ddf5bec75f2374b83fff
Andreas Brekken <a@abrkn.com>
2013-06-20 16:38:34 +0200

    Remove Q dependency from bitcoinAddress worker.

--- workers/lib/bitcoinaddress.js
+++ workers/lib/bitcoinaddress.js
@@ -1,56 +1,66 @@
 var debug = require('debug')('snow:bitcoinaddress')
-, Q = require('q')
+, async = require('async')
 , _ = require('lodash')
 
 var BitcoinAddress = module.exports = function(bitcoinEndpoint, dbClient) {
+    _.bindAll(this)
+
     var Bitcoin = require('bitcoin').Client
     this.bitcoin = new Bitcoin(bitcoinEndpoint)
-
     this.client = dbClient
-    this.loop().done()
+
+    this.loop()
 }
 
 BitcoinAddress.prototype.loop = function() {
     var that = this
-    return this.findAccountsNeedingAddresses()
-    .then(function(result) {
-        if (result.length) return that.loop()
-        debug('nothing to do, next loop in 10 sec')
-        return Q.delay(10e3)
-        .then(function() {
-            return that.loop()
-        })
+
+    async.waterfall([
+        this.getAccounts,
+        this.processAccounts
+    ], function(err) {
+        if (err) throw err
+        setTimeout(that.loop, 5e3)
     })
 }
 
-BitcoinAddress.prototype.saveAddress = function(accountId, address) {
-    var query = 'INSERT INTO btc_deposit_address (account_id, address) \
-                 VALUES ($1, $2)'
+BitcoinAddress.prototype.saveAddress = function(accountId, address, cb) {
+    console.log('assigning address %s to account %s', address, accountId)
 
-    return Q.ninvoke(this.client, 'query', {
-        text: query,
+    this.client.query({
+        text: [
+            'INSERT INTO btc_deposit_address (account_id, address)',
+            'VALUES ($1, $2)'
+        ].join('\n'),
         values: [accountId, address]
-    })
+    }, cb)
 }
 
-BitcoinAddress.prototype.processAccount = function(row) {
-    debug('processing %s', row.account_id)
-    return Q.ninvoke(this.bitcoin, 'getNewAddress')
-    .then(this.saveAddress.bind(this, row.account_id))
+BitcoinAddress.prototype.processAccounts = function(rows, cb) {
+    debug('processing %d work items', rows.length)
+    async.each(rows, this.processAccount, cb)
 }
 
-BitcoinAddress.prototype.findAccountsNeedingAddresses = function() {
+BitcoinAddress.prototype.processAccount = function(row, cb) {
     var that = this
-
-    var query = 'SELECT a.account_id \
-                 FROM account a \
-                 LEFT JOIN btc_deposit_address bda ON bda.account_id = a.account_id \
-                 WHERE a.currency_id = \'BTC\' AND bda.address IS NULL'
-
-    return Q.ninvoke(this.client, 'query', query)
-    .get('rows')
-    .then(function(rows) {
-        return rows.map(that.processAccount, that)
+    debug('processing %s', row.account_id)
+    this.bitcoin.getNewAddress(function(err, address) {
+        if (err) return cb(err)
+        that.saveAddress(row.account_id, address)
     })
-    .all()
+}
+
+BitcoinAddress.prototype.getAccounts = function(cb) {
+    debug('looking for work')
+
+    this.client.query({
+        text: [
+            'SELECT a.account_id',
+            'FROM account a',
+            'LEFT JOIN btc_deposit_address bda ON bda.account_id = a.account_id',
+            'WHERE a.currency_id = \'BTC\' AND bda.address IS NULL'
+        ].join('\n')
+    }, function(err, dr) {
+        cb(err, err ? null : dr.rows)
+    })
 }

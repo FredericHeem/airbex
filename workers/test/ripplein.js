f67d20087ff2d2b63e2afa08b4844d7eb1380ed3
Andreas Brekken <a@abrkn.com>
2013-06-23 14:16:40 +0200

    Remove obsolete ripplein tests.

--- workers/test/ripplein.js
+++ workers/test/ripplein.js
@@ -1,25 +1,5 @@
 var expect = require('expect.js')
-, RippleIn = require('../lib/ripplein')
+, RippleIn = require('../ripplein')
 
 describe('RippleIn', function() {
-    describe('cacheCurrencies', function() {
-        it('fetches all currencies', function(done) {
-            var ri = {
-                client: {
-                    query: function(text, cb) {
-                        expect(text).to.match(/from "currency/i)
-                        cb(null, { rows: [{ currency_id: 'XRP', scale: 6 }] })
-                    }
-                }
-            }
-
-            RippleIn.prototype.cacheCurrencies.call(ri)
-            .then(function() {
-                expect(ri.currencies['XRP']).to.be.ok()
-                expect(ri.currencies['XRP'].scale).to.be(6)
-                done()
-            })
-            .done()
-        })
-    })
 })

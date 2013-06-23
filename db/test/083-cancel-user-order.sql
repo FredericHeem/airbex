7a622140e0db9d74f4d68e686040c85912d99362
Andreas Brekken <a@abrkn.com>
2013-06-23 18:34:14 +0200

    Extract cancel user order. close #117

---
+++ db/test/083-cancel-user-order.sql
@@ -0,0 +1,31 @@
+BEGIN; SAVEPOINT before_tests;
+
+DO $$
+DECLARE
+    uid int;
+    oid int;
+    mid int;
+    vol bigint;
+BEGIN
+    uid := create_user('a@a', repeat('x', 64), FALSE);
+    mid := (SELECT market_id FROM market WHERE
+        base_currency_id || quote_currency_id = 'BTCNOK');
+    PERFORM edge_credit(uid, 'BTC', 1e8::bigint);
+    oid := create_order(uid, mid, 1, 700, '1');
+    PERFORM cancel_user_order(uid, oid);
+
+    SELECT volume
+    INTO vol
+    FROM "order"
+    WHERE order_id = oid;
+
+    IF NOT FOUND THEN
+        RAISE 'Order not found.';
+    END IF;
+
+    IF vol > 0 THEN
+        RAISE 'Order was not cancelled.';
+    END IF;
+END; $$; ROLLBACK TO before_tests;
+
+ROLLBACK;

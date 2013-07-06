302c28189ccc835981fd01f6d734045537f549fb
Andreas Brekken <a@abrkn.com>
2013-07-06 15:58:51 +0200

    Fix admin balances emitting scientific notation. close #211

--- api/admin/balances.js
+++ api/admin/balances.js
@@ -6,13 +6,16 @@ Balances.configure = function(app, conn, auth) {
 
 Balances.summary = function(conn, req, res, next) {
     conn.read.query([
-        'SELECT a.currency_id AS currency, a.type, SUM(a.balance / 10^c.scale) balance',
+        'SELECT a.currency_id currency, a.type, SUM(a.balance) balance',
         'FROM account a',
         'INNER JOIN "currency" c ON a.currency_id = c.currency_id',
         'GROUP BY a.currency_id, a.type',
         'ORDER BY type, currency'
     ].join('\n'), function(err, dr) {
         if (err) return next(err)
-        res.send(dr.rows)
+        res.send(dr.rows.map(function(row) {
+            row.balance = req.app.cache.formatCurrency(row.balance, row.currency)
+            return row
+        }))
     })
 }

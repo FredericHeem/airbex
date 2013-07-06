d55e4173fba4b03d244ae142075a7baba3956412
Andreas Brekken <a@abrkn.com>
2013-07-06 17:24:56 +0200

    Merge master and rename accordingly.

--- db/migrations/095-drop-simple.sql
+++ db/migrations/095-drop-simple.sql
@@ -50,14 +50,12 @@ CREATE OR REPLACE VIEW admin_user_view AS
     u.phone_number_verify_attempt_at, u.phone_number_unverified, u.fee_ratio,
     u.first_name, u.last_name, u.address, u.postal_area, u.city, u.country,
     u.email_verify_code, u.email_verify_code_issued_at, u.email_verified_at,
-    u.reset_email_code, u.reset_started_at, u.reset_phone_code,
-    u.language, u.tag, bda.address AS bitcoin_address,
-    lda.address AS litecoin_address
+    u.reset_email_code, u.reset_started_at, u.reset_phone_code, u.language,
+    u.tag, bda.address AS bitcoin_address, lda.address AS litecoin_address
    FROM "user" u
    LEFT JOIN ( SELECT a.user_id, bda.address
            FROM btc_deposit_address bda
       JOIN account a ON a.account_id = bda.account_id) bda ON bda.user_id = u.user_id
    LEFT JOIN ( SELECT a.user_id, lda.address
       FROM ltc_deposit_address lda
-   JOIN account a ON a.account_id = lda.account_id) lda ON lda.user_id = u.user_id
-  WHERE bda.user_id = u.user_id;
+   JOIN account a ON a.account_id = lda.account_id) lda ON lda.user_id = u.user_id;

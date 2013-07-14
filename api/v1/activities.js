a08acc45a7a43acdb8e668e1d1f281ed491ab909
Andreas Brekken <a@abrkn.com>
2013-07-14 14:54:16 +0200

    Add since-parameter to v1/activities. ref #245

--- api/v1/activities.js
+++ api/v1/activities.js
@@ -1,30 +1,79 @@
-var Q = require('q')
-, activities = module.exports = {}
+var activities = module.exports = {}
+, _ = require('lodash')
+, assert = require('assert')
 
 activities.configure = function(app, conn, auth) {
     app.get('/v1/activities', auth, activities.activities.bind(activities, conn))
 }
 
-activities.activities = function(conn, req, res, next) {
-    var query = [
-        'SELECT created, "type", details',
-        'FROM activity',
-        'WHERE user_id = $1',
-        'ORDER BY activity_id DESC',
-        'LIMIT 20'
-    ].join('\n')
+var detailWhitelist = {
+    AddBankAccount: ['accountNumber', 'iban', 'swiftbic', 'routingNumber'],
+    VerifyBankAccount: [],
+    RequestEmailVerification: [],
+    ChangePassword: [],
+    RemoveApiKey: [],
+    CreateApiKey: ['canTrade', 'canDeposit', 'canWithdraw'],
+    CreateOrder: ['market', 'type', 'price', 'amount', 'aon'],
+    CancelOrder: ['id'],
+    SendToUser: ['to', 'amount', 'currency', 'code'],
+    ReceiveFromUser: ['from', 'amount', 'currency'],
+    ConvertBid: ['market', 'amount'],
+    UpdateUser: [],
+    Created: [],
+    IdentitySet: [],
+    CreateVoucher: ['currency', 'amount'],
+    Withdraw: ['method', 'amount', 'currency', 'address'],
+    Credit: ['currency', 'amount'],
+    FillOrder: ['market', 'total', 'original', 'type', 'price'],
+    WithdrawComplete: ['amount', 'currency', 'method']
+}
 
-    Q.ninvoke(conn.read, 'query', {
-        text: query,
-        values: [req.user]
-    })
-    .then(function(dres) {
-        res.send(dres.rows.map(function(row) {
+activities.activities = function(conn, req, res, next) {
+    var query
+
+    if (req.query.since !== undefined) {
+        query = {
+            text: [
+                'SELECT activity_id id, created, "type", details',
+                'FROM activity_web',
+                'WHERE user_id = $1 AND activity_id > $2',
+                'ORDER BY activity_id ASC',
+                'LIMIT 20'
+            ].join('\n'),
+            values: [req.user, +req.query.since]
+        }
+    } else {
+        query = {
+            text: [
+                'SELECT activity_id id, created, "type", details',
+                'FROM activity_web',
+                'WHERE user_id = $1',
+                'ORDER BY activity_id DESC',
+                'LIMIT 20'
+            ].join('\n'),
+            values: [req.user]
+        }
+    }
+
+    conn.read.query(query, function(err, dr) {
+        if (err) return next(err)
+        res.send(dr.rows.map(function(row) {
             row.details = JSON.parse(row.details)
-            return row
+
+            var result = _.pick(row, 'type', 'created')
+
+            // Admin activities are sent as is
+            if (row.type.match(/^Admin/)) {
+                result.details = row.details
+            } else {
+                var whitelist = detailWhitelist[row.type]
+                assert(whitelist, 'No whitelist entry for ' + row.type)
+                result.details = _.pick(row.details, whitelist)
+            }
+
+            return result
         }))
-    }, next)
-    .done()
+    })
 }
 
 activities.log = function(conn, userId, type, details, retry) {

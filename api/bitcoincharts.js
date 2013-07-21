705781b73f48d71e1c0db3d2d91d4038d2166468
Andreas Brekken <a@abrkn.com>
2013-07-21 12:41:32 +0200

    Fix bitcoincharts to check base currency (might not be BTC)

--- api/bitcoincharts.js
+++ api/bitcoincharts.js
@@ -6,18 +6,21 @@ module.exports = exports = function(app) {
 exports.trades = function(req, res, next) {
     var since = req.query.since || 0
     req.app.conn.read.query({
-        text:
-            'SELECT ' +
-            'price_decimal::varchar price, ' +
-            'volume_decimal::varchar amount, ' +
-            'extract(epoch from om.created)::int date, ' +
-            'match_id tid ' +
-            'FROM match_view om ' +
-            'INNER JOIN "order" bo ON bo.order_id = om.bid_order_id ' +
-            'INNER JOIN market m ON m.market_id = bo.market_id ' +
-            'INNER JOIN "currency" bs ON m.base_currency_id = bs.currency_id ' +
-            'WHERE m.quote_currency_id = $1 AND om.match_id > $2 ' +
-            'ORDER BY om.match_id ASC;',
+        text: [
+            'SELECT',
+            '   price_decimal::varchar price,',
+            '   volume_decimal::varchar amount,',
+            '   extract(epoch from om.created)::int date,',
+            '   match_id tid',
+            'FROM match_view om',
+            'INNER JOIN "order" bo ON bo.order_id = om.bid_order_id',
+            'INNER JOIN market m ON m.market_id = bo.market_id',
+            'WHERE',
+            '   m.base_currency_id = \'BTC\' AND',
+            '   m.quote_currency_id = $1 AND',
+            '   om.match_id > $2',
+            'ORDER BY om.match_id ASC'
+        ].join('\n'),
         values: [req.params.currencyId, since]
     }, function(err, dr) {
         if (err) return next(err)
@@ -31,20 +34,22 @@ exports.orderbook = function(req, res, next) {
             'SELECT *',
             'FROM order_depth_view od',
             'INNER JOIN market m ON m.market_id = od.market_id',
-            'WHERE m.base_currency_id = \'BTC\' AND m.quote_currency_id = $1'
+            'WHERE',
+            '   m.base_currency_id = \'BTC\'',
+            '   m.quote_currency_id = $1'
         ].join('\n'),
         values: [req.params.currencyId]
     }, function(err, dr) {
         if (err) return next(err)
 
         res.send({
-            asks: dr.rows.filter(function(r) {
-                return r.side === 1
+            bids: dr.rows.filter(function(r) {
+                return r.side === 0
             }).map(function(r) {
                 return [r.price_decimal, r.volume_decimal]
             }),
-            bids: dr.rows.filter(function(r) {
-                return r.side === 0
+            asks: dr.rows.filter(function(r) {
+                return r.side === 1
             }).map(function(r) {
                 return [r.price_decimal, r.volume_decimal]
             })

0ddc4716cacb48607ca2d45dc6065ea3732d2d35
Andreas Brekken <a@abrkn.com>
2013-07-25 16:00:56 +0200

    Fix BDE, stop using email-existance.

--- api/test/verifyemail.js
+++ api/test/verifyemail.js
@@ -11,19 +11,12 @@ describe('verifyEmail', function() {
         var get = mock.once(request, 'get', function(opts, cb) {
             expect(opts.url).to.be('http://check.block-disposable-email.com/easyapi/json/key/rob.com')
             expect(opts.json).to.be(true)
-            cb(null, { statusCode: 200 }, { domain_state: 'ok' })
+            cb(null, { statusCode: 200 }, { domain_status: 'ok' })
         })
-
-        var existence = mock.once(require('email-existence'), 'check',
-            function(email, cb) {
-                cb(null, true)
-            }
-        )
 
         app.verifyEmail('bob@rob.com', function(err, res) {
             if (err) return done(err)
             expect(get.invokes).to.be(1)
-            expect(existence.invokes).to.be(1)
             expect(res).to.be(true)
             done()
         })
--- api/verifyemail.js
+++ api/verifyemail.js
@@ -1,41 +1,26 @@
-var async = require('async')
-, format = require('util').format
+var format = require('util').format
 , request = require('request')
 , config = require('konfu')
 , debug = require('debug')('snow:verifyemail')
-, emailExistence = require('email-existence')
 , urlFormat = 'http://check.block-disposable-email.com/easyapi/json/%s/%s'
 
 module.exports = function(email, cb) {
-    async.series([
-        function(next) {
-            var match = /^\S+@(\S+)$/.exec(email)
-            if (!match) return cb(new Error('Invalid email'))
-            var domain = match[1]
-            , url = format(urlFormat, config.bde_api_key, domain)
+    var match = /^\S+@(\S+)$/.exec(email)
+    if (!match) return cb(new Error('Invalid email'))
+    var domain = match[1]
+    , url = format(urlFormat, config.bde_api_key, domain)
 
-            request.get({
-                url: url,
-                json: true
-            }, function(err, res, data) {
-                if (err) return cb(err)
+    request.get({
+        url: url,
+        json: true
+    }, function(err, res, data) {
+        if (err) return cb(err)
 
-                if (res.statusCode != 200) {
-                    return cb(new Error(format('Status code %d', res.statusCode)))
-                }
-
-                debug('Email check for domain of %s:\n%j', email, data)
-                next(null, data.domain_status == 'ok')
-            })
-        },
-
-        function() {
-            debug('checking email existence')
-            emailExistence.check(email, function(err, exists) {
-                if (err) return cb(err)
-                debug('Email check for %s: %s', email, exists)
-                cb(null, exists)
-            })
+        if (res.statusCode != 200) {
+            return cb(new Error(format('Status code %d', res.statusCode)))
         }
-    ])
+
+        debug('Email check for domain of %s:\n%j', email, data)
+        cb(null, data.domain_status == 'ok')
+    })
 }

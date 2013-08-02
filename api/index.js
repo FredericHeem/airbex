f6a00813494e5fc7474b43d0716a595355071501
Andreas Brekken <a@abrkn.com>
2013-08-02 15:48:49 +0200

    Stop connecting to drop until its more stable.

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ if (module.parent) {
     app.ripple.drop = {}
 } else {
     app.notify = require('./email/notify')(app)
-    app.ripple.connect()
+    //app.ripple.connect()
 }
 
 var routes = ['bitcoincharts', 'v1', 'admin']

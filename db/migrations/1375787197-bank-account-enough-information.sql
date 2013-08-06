33269f8c317eddea163a8f0299196f6e4a40cf30
Andreas Brekken <a@abrkn.com>
2013-08-06 13:07:13 +0200

    Fix enough_information constraint of bank account.

---
+++ db/migrations/1375787197-bank-account-enough-information.sql
@@ -0,0 +1,8 @@
+ALTER TABLE bank_account DROP CONSTRAINT enough_information;
+
+-- Note that this constraint does not allow routing numbers.
+ALTER TABLE bank_account
+    ADD CONSTRAINT enough_information CHECK (
+        account_number IS NOT NULL AND iban IS NULL AND swiftbic IS NULL AND routing_number IS NULL OR
+        account_number IS NULL AND iban IS NOT NULL AND swiftbic IS NOT NULL AND routing_number IS NULL
+);

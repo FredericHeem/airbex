1a8a57d1544822ffaa44edfd63e51ed43ac04bd4
Sylvain Blot <sylvain@leetcoin.com>
2014-04-12 17:42:23 +0200

    new route

--- frontend/modules/deposit/routes.js
+++ frontend/modules/deposit/routes.js
@@ -7,23 +7,8 @@ module.exports = function(router, master, authorize) {
         if (!authorize.user()) return
         master(require('./bitcoin')(), 'deposit')
     })
-    .add(/^deposit\/litecoin$/, function() {
+    .add(/^deposit\/logos$/, function() {
         if (!authorize.user()) return
-        master(require('./litecoin')(), 'deposit')
-    })  
-    .add(/^deposit\/bank$/, function() {
-        if (!authorize.user(2)) return
-        master(require('./bank')(), 'deposit')
-    })
-    .add(/^deposit\/bank\/(USD|EUR|CHF)$/, function(currency) {
-        if (!authorize.user(3)) return
-
-        if (currency == 'USD') {
-            master(require('./bank/USD')(), 'deposit')
-        } else if (currency == 'EUR') {
-            master(require('./bank/EUR')(), 'deposit')
-        } else if (currency == 'CHF') {
-            master(require('./bank/CHF')(), 'deposit')
-        }
+        master(require('./logos')(), 'deposit')
     })
 }

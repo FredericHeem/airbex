0a1131494fb9b9f675da6e771821ded562e64b63
Frederic Heem <frederic.heem@gmail.com>
2014-04-13 12:11:22 +0200

    add role base for openssh

---
+++ chef/roles/base.rb
@@ -0,0 +1,22 @@
+name "base"
+description "Essential recipes for securing every server"
+run_list(
+    "recipe[openssh]",
+    "recipe[chef-client::delete_validation]"
+)
+override_attributes({
+  "openssh" => {
+     "server" => {
+      "allow_agent_forwarding" => "no",
+      "allow_tcp_forwarding" =>  "no",
+      "client_alive_count_max" =>  "0",
+      "client_alive_interval" =>  "600",
+      "ignore_user_known_hosts" =>  "yes",
+      "login_grace_time" =>  "30s",
+      "password_authentication" =>  "yes",
+      "permit_root_login" =>  "yes",
+      "rsa_authentication" =>  "no"
+     }
+  }
+})
+

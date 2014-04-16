aed4c84ecc341e6836ba68642499c85109b29f9e
Frederic Heem <frederic.heem@gmail.com>
2014-04-16 23:25:23 +0200

    add secrets-sbex.sh secrets-logos.sh

---
+++ chef/scripts/secrets-logos.sh
@@ -0,0 +1,9 @@
+export ORGNAME=logos
+export AWS_SSH_KEY_ID=mycompany
+export AWS_ACCESS_KEY_ID=ABCABCABCABC
+export AWS_SECRET_ACCESS_KEY=ABCABCABCABCABC/DEFDEFDEFDEF
+export KNIFE_USER=logosx
+export CHEF_SERVER_URL=
+export KNIFE_ENV=dev
+export EDITOR=vim
+
---
+++ chef/scripts/secrets-sbex.sh
@@ -0,0 +1,9 @@
+export ORGNAME=sbex
+export AWS_SSH_KEY_ID=mycompany
+export AWS_ACCESS_KEY_ID=ABCABCABCABC
+export AWS_SECRET_ACCESS_KEY=ABCABCABCABCABC/DEFDEFDEFDEF
+export KNIFE_USER=alainproviste
+export CHEF_SERVER_URL=https://api.opscode.com/organizations/$ORGNAME
+export KNIFE_ENV=dev
+export EDITOR=vim
+

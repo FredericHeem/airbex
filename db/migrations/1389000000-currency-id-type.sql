799c5f2b9038e9f184fb3bc73cfd3288c4425429
Frederic Heem <frederic.heem@gmail.com>
2014-04-24 15:03:17 +0200

    change order of DROP VIEW

--- db/migrations/1389000000-currency-id-type.sql
+++ db/migrations/1389000000-currency-id-type.sql
@@ -4,8 +4,8 @@ DROP VIEW IF EXISTS account_transaction_view;
 DROP VIEW IF EXISTS active_order_view;
 DROP VIEW IF EXISTS order_depth_view;
 DROP VIEW IF EXISTS transaction_view;  
-DROP VIEW IF EXISTS order_view;
 DROP VIEW IF EXISTS market_summary_view;
+DROP VIEW IF EXISTS order_view;
 DROP VIEW IF EXISTS match_view;
 
 ALTER TABLE currency

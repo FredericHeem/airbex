d490c99b29aade8a29d453d9126cf237e370aa93
Frederic Heem <frederic.heem@gmail.com>
2014-05-07 22:51:35 +0200

    update logosd

--- chef/site-cookbooks/snow/recipes/logosd.rb
+++ chef/site-cookbooks/snow/recipes/logosd.rb
@@ -3,6 +3,6 @@ compilecrypto "logosd" do
     deamonName "logosd"
     cryptoCode "lgs"
     gitRepo "https://github.com/sylvainblot/logos"
-    gitRef "2e9c5e9404654e9a99a2d52b24137b6c249c253d"
+    gitRef "898887e396a0fc9ec8250a329020987f265b9ab3"
     compileCommand "rm -rf src/logosd && cd src && sudo make -f makefile.unix"
 end

587daf45e62cd0ec5d9850ff93b99d97823e4a02
Frederic Heem <frederic.heem@gmail.com>
2014-05-09 00:48:30 +0200

    set limit for BTCCHF and BTCEUR

--- db/sbex/1389599000-minmax.sql
+++ db/sbex/1389599000-minmax.sql
@@ -1,15 +1,15 @@
 update market
  set askminvolume=100000,
-     askmaxprice=100000000,
+     askmaxprice=500000000,
      bidminvolume=100000,
-     bidminprice=1000,
-     name='LTCBTC'
- where base_currency_id='LTC' and quote_currency_id='BTC';
+     bidminprice=1000000,
+     name='BTCEUR'
+ where base_currency_id='BTC' and quote_currency_id='EUR';
  
  update market
- set askminvolume=100000000,
-     askmaxprice=10000,
-     bidminvolume=100000000,
-     bidminprice=10,
-     name='DOGEBTC'
- where base_currency_id='DOGE' and quote_currency_id='BTC';
+ set askminvolume=100000,
+     askmaxprice=500000000,
+     bidminvolume=100000,
+     bidminprice=1000000,
+     name='BTCCHF'
+ where base_currency_id='BTC' and quote_currency_id='CHF';

0bf81c0ecb299b0a52897fc61025e4959273e61a
Frederic Heem <frederic.heem@gmail.com>
2014-06-08 15:36:24 +0200

    allow_tcp_forwarding to yes

--- chef/roles/base.rb
+++ chef/roles/base.rb
@@ -14,7 +14,7 @@ override_attributes({
   "openssh" => {
      "server" => {
       "allow_agent_forwarding" => "no",
-      "allow_tcp_forwarding" =>  "no",
+      "allow_tcp_forwarding" =>  "yes",
       "client_alive_count_max" =>  "0",
       "client_alive_interval" =>  "1200",
       "ignore_user_known_hosts" =>  "yes",

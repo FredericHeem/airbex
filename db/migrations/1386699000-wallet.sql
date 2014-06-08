32815547885ec8802839056c270205d2ac753185
Frederic Heem <frederic.heem@gmail.com>
2014-06-08 15:37:41 +0200

    table wallet:balance and height default

--- db/migrations/1386699000-wallet.sql
+++ db/migrations/1386699000-wallet.sql
@@ -1,8 +1,8 @@
 -- wallet
 CREATE TABLE wallet (
    currency_id currency_id NOT NULL,
-   height int,
-   balance decimal(16, 8)
+   height int DEFAULT 0,
+   balance decimal(16, 8) DEFAULT 0
 );
 
 INSERT INTO wallet(currency_id, height, balance) values('BTC', 0, 0);

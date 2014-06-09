65b2166a93eb29ff648e8428100b10db2426a075
Frederic Heem <frederic.heem@gmail.com>
2014-06-09 12:57:45 +0200

    password_authentication no, permit_root_login no

--- chef/roles/base.rb
+++ chef/roles/base.rb
@@ -13,14 +13,14 @@ run_list(
 override_attributes({
   "openssh" => {
      "server" => {
-      "allow_agent_forwarding" => "no",
+      "allow_agent_forwarding" => "yes",
       "allow_tcp_forwarding" =>  "yes",
       "client_alive_count_max" =>  "0",
       "client_alive_interval" =>  "1200",
       "ignore_user_known_hosts" =>  "yes",
       "login_grace_time" =>  "30s",
-      "password_authentication" =>  "yes",
-      "permit_root_login" =>  "yes",
+      "password_authentication" =>  "no",
+      "permit_root_login" =>  "no",
       "rsa_authentication" =>  "no"
      }
   },

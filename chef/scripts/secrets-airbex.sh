b4402a5ad654eca978cf9350193ba871169445b1
Frederic Heem <frederic.heem@gmail.com>
2014-06-27 23:31:05 +0200

    change chef url

--- chef/scripts/secrets-airbex.sh
+++ chef/scripts/secrets-airbex.sh
@@ -1,6 +1,9 @@
 export ORGNAME=airbex
+export AWS_SSH_KEY_ID=mycompany
+export AWS_ACCESS_KEY_ID=ABCABCABCABC
+export AWS_SECRET_ACCESS_KEY=ABCABCABCABCABC/DEFDEFDEFDEF
 export KNIFE_USER=fredericheem
-export CHEF_SERVER_URL=https://api.opscode.com/organizations/$ORGNAME
+export CHEF_SERVER_URL=https://chef.airbex.net
 export KNIFE_ENV=demo
 export EDITOR=vim
 

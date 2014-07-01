b6a0a96bdc668915edeed2241f2fe264be11aa09
Frederic Heem <frederic.heem@gmail.com>
2014-07-01 22:51:54 +0200

    install ipset

--- chef/site-cookbooks/snow/recipes/firewall.rb
+++ chef/site-cookbooks/snow/recipes/firewall.rb
@@ -3,6 +3,9 @@ include_recipe "snow::common"
 bag = Chef::EncryptedDataBagItem.load("snow", 'main')
 env_bag = bag[node.chef_environment]
 
+package 'ipset' do
+end
+
 reverse_ip = "127.0.0.1"
 if env_bag['reverse']
   reverse_ip = env_bag['reverse']['ip'] || "127.0.0.1"

19e3d269ce70870f9fbf462a71bd0faba985fcb8
Frederic Heem <frederic.heem@gmail.com>
2014-10-03 11:27:06 +0200

    code coverage

---
+++ brushtest/test/testBitcoincharts.js
@@ -0,0 +1,52 @@
+/*global describe, it, before, after*/
+var assert = require('assert');
+var request = require('supertest');
+var debug = require('debug')('testCharts')
+var config = require('./configTest.js')();
+var TestMngr = require('./TestMngr');
+
+describe('Charts', function () {
+    "use strict";
+    
+    var testMngr = new TestMngr(config);
+    var snowBot = testMngr.bot();
+    var snowChef = testMngr.chef();
+    var clientAdmin = testMngr.client("admin");
+    var client = testMngr.client("alice");
+    var clientConfig = testMngr.clientConfig("alice");
+    var clientBob = testMngr.client("bob");
+    var currency = 'btc';
+    
+    before(function(done) {
+        testMngr.start().then(done).fail(done);
+    });
+    
+    describe('ChartsPublic', function () {
+        it('ChartsPublicTrade', function (done) {
+            client.get('bitcoincharts/' + currency + '/trades.json')
+            .then(function(result){
+                assert(result)
+                console.log(result)
+                done()
+            });
+        });
+        it('ChartsPublicOrderbook', function (done) {
+            client.get('bitcoincharts/' + currency + '/orderbook.json')
+            .then(function(result){
+                assert(result)
+                console.log(result)
+                done()
+            });
+        });
+    });
+    
+    describe('ChartsAuth', function () {
+        before(function(done) {
+            debug("before");
+            this.timeout(5 * 1000);
+            testMngr.login().then(done).fail(done);
+        });
+
+    });
+   
+});
---
+++ brushtest/test/testLanguage.js
@@ -0,0 +1,87 @@
+/*global describe, it, before, after*/
+var assert = require('assert');
+var debug = require('debug')('testLanguage')
+var config = require('./configTest.js')();
+var TestMngr = require('./TestMngr');
+var request = require('request');
+
+describe('Language', function () {
+    "use strict";
+    
+    var testMngr = new TestMngr(config);
+    var snowBot = testMngr.bot();
+    var snowChef = testMngr.chef();
+    var clientAdmin = testMngr.client("admin");
+    var client = testMngr.client("alice");
+    var clientConfig = testMngr.clientConfig("alice");
+    var clientBob = testMngr.client("bob");
+    var currency = 'btc';
+    
+    before(function(done) {
+        testMngr.start().then(done).fail(done);
+    });
+    
+    describe('LanguagePublic', function () {
+        it('LanguagePublicNoAcceptLanguage', function (done) {
+            client.get('v1/language')
+            .then(function(result){
+                assert(result)
+                console.log(result)
+                assert(!result.language)
+                done()
+            }).fail(done);
+        });
+        it('LanguagePublicAcceptLanguageEmpty', function (done) {
+            request.get(config.url + 'v1/language', 
+                    {headers:{'Accept-Language':''}}, function(err, res, body) {
+                assert(!err)
+                console.log(body)
+                assert(!body.language)
+                done();
+            })
+        });
+        it('LanguagePublicAcceptInvalid', function (done) {
+            request.get(config.url + 'v1/language', 
+                    {headers:{'Accept-Language':'aaaaaaaaaaaaaaaaaaaaa'}}, function(err, res, body) {
+                assert(!err)
+                console.log(body)
+                assert(!body.language)
+                done();
+            })
+        });
+        it('LanguagePublicAcceptLanguageA', function (done) {
+            request.get(config.url + 'v1/language', 
+                    {headers:{'Accept-Language':'a'}}, function(err, res, body) {
+                assert(!err)
+                console.log(body)
+                done();
+            })
+        });
+        
+        it('LanguagePublicAcceptLanguageFr', function (done) {
+            request.get(config.url + 'v1/language', 
+                    {headers:{'Accept-Language':'fr'}}, function(err, res, body) {
+                assert(!err)
+                console.log(body)
+                done();
+            })
+        });
+        it('LanguagePublicAcceptLanguageFr', function (done) {
+            request.get(config.url + 'v1/language', 
+                    {headers:{'Accept-Language':'fr,it-IT;q=0.8,it;q=0.6,en-US;q=0.4,en;q=0.2'}}, function(err, res, body) {
+                assert(!err)
+                console.log(body)
+                done();
+            })
+        });
+    });
+    
+    describe('LanguageAuth', function () {
+        before(function(done) {
+            debug("before");
+            this.timeout(5 * 1000);
+            testMngr.login().then(done).fail(done);
+        });
+    });
+   
+});

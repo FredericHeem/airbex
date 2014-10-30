3f0d334cb45d2c84f55cd0aebb3ea67323d8a0d1
Frederic Heem <frederic.heem@gmail.com>
2014-10-30 17:01:26 +0100

    update darkcoin

--- chef/site-cookbooks/snow/recipes/darkcoind.rb
+++ chef/site-cookbooks/snow/recipes/darkcoind.rb
@@ -3,6 +3,6 @@ compilecrypto "darkcoind" do
     deamonName "darkcoind"
     cryptoCode "drk"
     gitRepo "git@github.com:darkcoinproject/darkcoin.git"
-    gitRef "d9f6def36abf765a21301614626fe320406a81c5"
+    gitRef "v0.10.15.17"
     compileCommand "cd src && sudo make -f makefile.unix"
 end
